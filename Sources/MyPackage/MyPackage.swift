34f6446477a702bad597e8c45027934d3f41428a
zhangchuanqi <zhangchuanqi@okay.cn>
2020-07-27 17:43:48 +0800

    Initial Commit

--- Sources/MyPackage/MyPackage.swift
+++ Sources/MyPackage/MyPackage.swift
@@ -1,7 +1,9 @@
 struct MyPackage {
     var text = "Hello, World!"
-    
-    
+    var name = "kkk"
+    init(name:String) {
+        self.name = name
+    }
     
     func addtest(_ a:Int, _ b:Int) -> Int {
         return a + b
--- Tests/MyPackageTests/MyPackageTests.swift
+++ Tests/MyPackageTests/MyPackageTests.swift
@@ -6,7 +6,9 @@ final class MyPackageTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(MyPackage().text, "Hello, World!")
+        XCTAssertEqual(MyPackage(name: "111").text, "Hello, World!")
+        let aa = MyPackage.init(name: "111")
+        XCTAssertEqual(aa.addtest(5, 3),8)
     }
 
     static var allTests = [
